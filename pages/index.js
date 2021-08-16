fcd82c45922a3441ef94e918433863ab52f12d8f
Joseph David <techiejossy@gmail.com>
2021-08-16 22:41:30 +0100

    feat: Fetch user's repositories

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,17 @@ function Home() {
     const [userDetails, setUserDetails] = useState(null)
 
     if (userDetails) {
-        console.log(userDetails.profile)
+        console.log(userDetails.repos)
     } else {
         console.log('hey')
     }
+
+    // userDetails.repos.map(repo => {
+
+    //     console.log(repo.name)
+
+
+    // })
 
 
     return (
@@ -33,15 +40,28 @@ function Home() {
                 {userDetails ? (
                     <div id="search-result-success" className="mt-5 container">
 
+                        {userDetails.repos.map(repo => {
+
+                            <h1>{repo.name}</h1>
+
+
+                        })}
+
                         <div id="user-avatar" className="container flex justify-center mb-2">
                             <img src={userDetails.profile.avatar_url} alt="" className="rounded-full h-20" />
                         </div>
-                        <div className="user-info">
+                        <div className="user-profile-info">
                             <h2 className="text-2xl font-bold">{userDetails.profile.name}</h2>
                             <p className="text-sm italic">{userDetails.profile.bio}</p>
+                            <p className="text-sm italic">{userDetails.profile.followers} followers</p>
                         </div>
 
+                        <h1>{userDetails.profile.name}'s Repositories</h1>
+
+
+
                     </div>
+
                 ) : <p className="mt-5">start searching 🥱</p>}
 
             </div>
